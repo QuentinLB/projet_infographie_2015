8957340ed198ad779f34965da08188577481be45
QuentinLB <q.lebour@gmail.com>
2015-02-24 08:36:54 -0500

    deplacement en (x,y,z) avec le clavier

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -21,6 +21,10 @@ ofLight light;
 CubeMap cubemap;
 ofCamera cam;
 
+bool CAMERA_TRACKING = false;
+int CAMERA_VIT = 5;
+float SENSITIVITY = 0.25;
+
 //--------------------------------------------------------------
 void ofApp::setup(){
 
@@ -52,8 +56,8 @@ void ofApp::draw(){
 	//cubemap.draw(cam);
 	gui.draw();
 
-	cam.setPosition(gui.getCamLocation());
-	cam.lookAt(ofVec3f(ofGetWidth()*.5, ofGetHeight()*.5, 0), ofVec3f(0, 1, 0));
+	//cam.setPosition(gui.getCamLocation());
+	//cam.lookAt(ofVec3f(ofGetWidth()*.5, ofGetHeight()*.5, 0), ofVec3f(0, 1, 0));
 
 	light.setPosition(gui.getSunCenter());
 	light.setDiffuseColor(gui.getSunColor());
@@ -73,17 +77,62 @@ void ofApp::draw(){
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+	ofVec3f pos = cam.getPosition();
+	if(key == 'z' || key == 'w' || key == OF_KEY_UP)
+		pos.z = pos.z - CAMERA_VIT;
+	else if(key == 's' || key == OF_KEY_DOWN)
+		pos.z = pos.z + CAMERA_VIT;
+	else if(key == 'q' || key =='a' || key == OF_KEY_LEFT)
+		pos.x = pos.x - CAMERA_VIT;
+	else if(key == 'd' || key == OF_KEY_RIGHT)
+		pos.x = pos.x + CAMERA_VIT;
+	else if(key == ' ')
+		pos.y = pos.y + CAMERA_VIT;
+	else if(key == OF_KEY_CONTROL)
+		pos.y = pos.y - CAMERA_VIT;
+	cam.setPosition(pos);
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
-
+	switch (key)
+	{
+	case OF_KEY_ALT:
+		CAMERA_TRACKING != CAMERA_TRACKING;
+	default:
+		break;
+	}
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ){
+	//int centered_x = x - ofGetWidth() * 0.5;
+	//int centered_y = y - ofGetWidth() * 0.5;
+	//int middle_x = ofGetWidth() * 0.5;
+	//int middle_y = ofGetHeight() * 0.5;
+	//ofVec3f mouse_dir(0);
+ //   static double CurrentRotationX = 0.0;
 
+	//mouse_dir.x = (middle_x - x)/SENSITIVITY; 
+	//mouse_dir.y = (middle_y - y)/SENSITIVITY;
+
+ //   CurrentRotationX += mouse_dir.y;
+
+	//// get the axis to rotate around the x-axis. 
+ //   Vector Axis = CrossProduct(View - Position, Up);
+ //   // To be able to use the quaternion conjugate, the axis to
+ //   // rotate around must be normalized.
+ //   Axis = Normalize(Axis);
+
+ //   // Rotate around the y axis
+ //   RotateCamera(MouseDirection.y, Axis.x, Axis.y, Axis.z);
+ //   // Rotate around the x axis
+ //   RotateCamera(MouseDirection.x, 0, 1, 0);
+
+	//ofVec3f pos = cam.getPosition();
+	//ofVec3f orient = cam.getOrientationEuler();
+	
+	//cam.lookAt(ofVec3f(y, 0, x), ofVec3f(0, 1, 0));
 }
 
 //--------------------------------------------------------------
