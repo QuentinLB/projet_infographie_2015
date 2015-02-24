bf3b105a99db268205439e5f6e091c853efa091a
QuentinLB <q.lebour@gmail.com>
2015-02-24 15:44:58 -0500

    petite avancée sur la gestion de l'orientation de la camera avec la souris

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -23,7 +23,7 @@ ofCamera cam;
 
 bool CAMERA_TRACKING = false;
 int CAMERA_VIT = 5;
-float SENSITIVITY = 0.25;
+float SENSITIVITY = 0.1;
 
 //--------------------------------------------------------------
 void ofApp::setup(){
@@ -56,8 +56,8 @@ void ofApp::draw(){
 	//cubemap.draw(cam);
 	gui.draw();
 
-	//cam.setPosition(gui.getCamLocation());
-	//cam.lookAt(ofVec3f(ofGetWidth()*.5, ofGetHeight()*.5, 0), ofVec3f(0, 1, 0));
+	cam.setPosition(gui.getCamLocation());
+	cam.lookAt(ofVec3f(ofGetWidth()*.5, ofGetHeight()*.5, 0), ofVec3f(0, 1, 0));
 
 	light.setPosition(gui.getSunCenter());
 	light.setDiffuseColor(gui.getSunColor());
@@ -73,6 +73,7 @@ void ofApp::draw(){
 
 	cam.end();
 	ofDisableLighting();
+	ofDrawBitmapString(ofToString(ofGetFrameRate()), 10, 15);
 }
 
 //--------------------------------------------------------------
@@ -104,36 +105,85 @@ void ofApp::keyReleased(int key){
 	}
 }
 
+//ofVec3f rotateCamera(float angle, ofVec3f rotation, ofVec3f cam_orientation)
+//{
+//	ofQuaternion temp(
+//		rotation.x * sin(angle/2),
+//		rotation.y * sin(angle/2),
+//		rotation.z * sin(angle/2),
+//		cos(angle/2)
+//	);
+//
+//	ofQuaternion quat_view(
+//		cam_orientation.x,
+//		cam_orientation.y,
+//		cam_orientation.z,
+//		0
+//	);
+//
+//	ofQuaternion result = (temp * quat_view)*temp.conj();
+//	
+//	return ofVec3f(result.x(),result.y(),result.z());
+//}
+
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ){
-	//int centered_x = x - ofGetWidth() * 0.5;
-	//int centered_y = y - ofGetWidth() * 0.5;
-	//int middle_x = ofGetWidth() * 0.5;
-	//int middle_y = ofGetHeight() * 0.5;
-	//ofVec3f mouse_dir(0);
- //   static double CurrentRotationX = 0.0;
+	ofVec3f pos = cam.getPosition();
+	ofVec3f orient = cam.getOrientationEuler();
+	ofVec3f up = cam.getUpDir();
 
-	//mouse_dir.x = (middle_x - x)/SENSITIVITY; 
-	//mouse_dir.y = (middle_y - y)/SENSITIVITY;
+	int middle_x = ofGetWidth() /2;
+	int middle_y = ofGetHeight() /2;
 
- //   CurrentRotationX += mouse_dir.y;
+	int centered_x = x - middle_x;
+	int centered_y = middle_y - y;
+	ofLog(OF_LOG_NOTICE,"x,y : (%d,%d)",centered_x,centered_y);
+	ofVec3f mouse_dir(0);
+    static double CurrentRotationX = 0.0;
 
-	//// get the axis to rotate around the x-axis. 
- //   Vector Axis = CrossProduct(View - Position, Up);
- //   // To be able to use the quaternion conjugate, the axis to
- //   // rotate around must be normalized.
- //   Axis = Normalize(Axis);
+	// The maximum angle we can look up or down, in radians
+	double maxAngle = 1;
 
- //   // Rotate around the y axis
- //   RotateCamera(MouseDirection.y, Axis.x, Axis.y, Axis.z);
- //   // Rotate around the x axis
- //   RotateCamera(MouseDirection.x, 0, 1, 0);
+	// if the mouse hasn't moved, return without doing
+	// anything to our view
+	if(centered_x == 0 && centered_y == 0)
+		return;
 
-	//ofVec3f pos = cam.getPosition();
-	//ofVec3f orient = cam.getOrientationEuler();
-	
-	//cam.lookAt(ofVec3f(y, 0, x), ofVec3f(0, 1, 0));
+	// otherwise move the mouse back to the middle of the screen
+	SetCursorPos(ofGetWindowPositionX()+middle_x, ofGetWindowPositionY()+middle_y);
+
+	mouse_dir.x = centered_x/SENSITIVITY; 
+	mouse_dir.y = centered_y/SENSITIVITY;
+
+    CurrentRotationX += mouse_dir.y;
+ 
+	// We don't want to rotate up more than one radian, so we cap it.
+	if(CurrentRotationX > 1)
+	{
+		CurrentRotationX = 1;
+		return;
+	}
+	// We don't want to rotate down more than one radian, so we cap it.
+	if(CurrentRotationX < -1)
+	{
+		CurrentRotationX = -1;
+		return;
+	}
+	else
+	{
+		// get the axis to rotate around the x-axis. 
+		ofVec3f axis = (orient - pos).cross(up).normalize();
+		// Rotate around the y axis
+		orient = rotateCamera(mouse_dir.y, axis, orient);
+		// Rotate around the x axis
+		orient = rotateCamera(mouse_dir.x, axis, orient);
+		
+		ofLog(OF_LOG_NOTICE, "toto");
+		cam.lookAt(orient, up);
+	}
 }
+
+
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
