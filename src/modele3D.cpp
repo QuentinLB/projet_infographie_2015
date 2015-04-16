5e0fa003e05d6aa61ee44c716ea2741568ff3098
AlexandreChouinard07 <alex.chouinard07@hotmail.com>
2015-04-16 17:22:15 -0400

    Update modele3D.cpp
    ajout methode pour le vaisseau

--- src/modele3D.cpp
+++ src/modele3D.cpp
@@ -12,17 +12,37 @@ modele3D::modele3D(std::string pathFile, float posX, float posY, float posZ, flo
 	cheminFichier = pathFile;
 	mod3DposX = posX;
 	mod3DposY = posY;
-	mod3DposZ =	posZ;
+	mod3DposZ = posZ;
 	mod3DscaleX = scaleX;
 	mod3DscaleY = scaleY;
 	mod3DscaleZ = scaleZ;
+	mod3DrotationX = 0;
+	mod3DrotationY = 0;
+	mod3DrotationZ = 0.0;
 }
-
 void modele3D::setup(){
 	model.loadModel(cheminFichier);
-	model.setPosition(mod3DposX, mod3DposY, mod3DposZ);
+	setModel();
 	model.setScale(mod3DscaleX,mod3DscaleY,mod3DscaleZ);
-	model.setRotation(0,0, 45, 45,45);
+}
+
+void modele3D::setElement(ofNode* aNode)
+{
+	ofVec3f pos = aNode->getGlobalPosition();
+
+	mod3DposX = pos.x;
+	mod3DposY = pos.y;
+	mod3DposZ = pos.z;
+
+	
+	ofVec3f orientation = aNode->getOrientationEuler();
+	mod3DrotationX = orientation.x;//x
+	mod3DrotationY = orientation.y;//z
+	mod3DrotationZ = orientation.z ;//y
+	
+	//std::cout << "orientationX : " << orientation.x << endl;
+	//std::cout << "orientationY : " << orientation.y << endl;
+	//std::cout << "orientationZ : " << orientation.z << endl;
 }
 
 void modele3D::update(){
@@ -31,6 +51,18 @@ void modele3D::update(){
 
 void modele3D::draw(){
 	ofPushMatrix();
+	setModel();
 	model.drawFaces();
 	ofPopMatrix();
+}
+
+void modele3D::setModel()
+{
+	model.setPosition(mod3DposX, mod3DposY, mod3DposZ);
+	//model.setRotation(0, , -1, 0, 0); //x		le modele n'est pas sur la bonne base ortho normal
+	//model.setRotation(1, 180, 0, 1, 0); //z
+
+	model.setRotation(2, mod3DrotationX + 60, -1, 0, 0); //x 
+	model.setRotation(3, mod3DrotationY, 0, 0, -1); //y
+	model.setRotation(4, 180, 0, 1, 0); //z
 }
